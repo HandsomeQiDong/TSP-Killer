e567206fdce6864706d97e2bb82514c6728359f9
HandsomeQiDong <76278414+HandsomeQiDong@users.noreply.github.com>
2022-10-14 23:34:36 +0800

    version 2.1
    在2.0的基础上，对数据可视化做了一些改进。

--- SA+tabu KILLS TSP 2.0.py
+++ SA+tabu KILLS TSP 2.0.py
@@ -17,8 +17,8 @@ T_end = 15      #结束温度
 alpha = 0.99        #降温系数
 L = 2000        #迭代次数
 buildings = 19  #楼栋数量
-Demand_matrix_root = "D:\demo\yiwenchao finnal course\Demand matrix.xlsx"       #
-Distance_matrix_root = "D:\demo\yiwenchao finnal course\Distance matrix.xlsx"   #
+Demand_matrix_root = "D:\demo\yiwenchao finnal course\Demand matrix.xlsx"       #修改成Demand matrix.xlsx在自己电脑的路径
+Distance_matrix_root = "D:\demo\yiwenchao finnal course\Distance matrix.xlsx"   #同上
 
 #表格数据读取函数 输入：需求文件路径，距离文件路径
 def read_excel_data(demand_root,distance_root):
@@ -86,15 +86,16 @@ def totaldemand(v,travel_route,Distance_matrix,Demand_matrix):
 #计算适应度函数(合理吗？？？？？)
 def fitness(route_distance, sum_demand):
     all = route_distance + sum_demand
-    return 0.3*route_distance-0.7*sum_demand
+    return 0.3*route_distance-7*sum_demand
 
 #模拟退火算法
-def SA(T_begin,T_end,alpha,L,Distance_matrix,Demand_matrix):                                  #T_begin初始温度;T_end终止温度;alpha降温系数;L迭代次数
+def SA(T_begin,T_end,alpha,L,Distance_matrix,Demand_matrix):                    #T_begin初始温度;T_end终止温度;alpha降温系数;L迭代次数
     tabu_list = []                                                              #空禁忌列表
     old_route = init_route()                                                    #产生初始路径route0赋值给old_route
     T = T_begin                                                                 #当前温度等于初始温度
     cnt = 0                                                                     #计数器 用于输出报文时的序号 “第 cnt 次 ...”
     Shortestlength_list = []                                                    #记录每种温度情况下得到的最短路径距离
+    Fitness_list = []                                                           #记录每种温度情况下得到的最小适应度
     while T > T_end:                                                            #如果当前温度大于终止温度
         for i in range(L):                                                      #循环L次
             new_route,tabu_list = creat_newRoute(old_route,tabu_list)           #扰动产生新路径
@@ -105,50 +106,60 @@ def SA(T_begin,T_end,alpha,L,Distance_matrix,Demand_matrix):                    
             old_fitness = fitness(old_dist,totaldemand(10,old_route,Distance_matrix,Demand_matrix))
             new_fitness = fitness(new_dist,totaldemand(10,new_route,Distance_matrix,Demand_matrix))
             delta_fitness = new_fitness - old_fitness
-
-###########################################################################################################
-            if delta_fitness >= 0:                                                 #如果差值≥0，说明新路线比旧路线长，按一定概率接受新路线
+            if delta_fitness >= 0:                                              #如果差值≥0，说明新路线比旧路线长，按一定概率接受新路线
                 rand = random.uniform(0,1)
                 if rand < math.exp(-delta_fitness/T):
                     old_route = new_route
             else:                                                               #否则，必然接受新路线
                 old_route = new_route
-############################################################################################################
         T = T * alpha                                                           #降温
         cnt += 1                                                                #计数器
         Shortestlength = totaldistance(old_route,Distance_matrix)               #当前路径长度（更新后的旧路径）
-        Shortestlength_list.append(Shortestlength)                              #把当前路径长度加入“趋势”列表
-        print(cnt,"次降温，温度为：",T," 路程长度为：", Shortestlength,"适应度：",fitness(Shortestlength,totaldemand(10,old_route,Distance_matrix,Demand_matrix)))               #报文
+        Shortestlength_list.append(Shortestlength)                              #把当前路径长度加入列表
+        Fitness_list.append(fitness(Shortestlength,totaldemand(10,old_route,Distance_matrix,Demand_matrix)))
+                                                                                #把当前适应度加入列表
+        # print(cnt,"次降温，温度为：",T," 路程长度为：", Shortestlength,"适应度：",fitness(Shortestlength,totaldemand(10,old_route,Distance_matrix,Demand_matrix)))
+                                                                                #报文
     optimal_route = old_route                                                   #当温度降到终止温度，把此时的旧路线作为最优路线
     optimal_distance = totaldistance(optimal_route,Distance_matrix)             #计算最优路线的路程长度
+    optimal_demand = totaldemand(10,old_route,Distance_matrix,Demand_matrix)
+    optimal_fitness = fitness(optimal_distance,optimal_demand)
     print("本次独立运行 最优路径长度：",optimal_distance)                            #打印最优路径长度
     print("本次独立运行 最优路线：",optimal_route)                                  #打印最优路线
-    print("本次独立运行 满足需求量：",totaldemand(10,old_route,Distance_matrix,Demand_matrix))
-    plt.plot(Shortestlength_list)                                               #绘制单次运行若干次降温的最优长度折线图
-    plt.savefig("SA+tabu KILLS TSP A single run.png")                           #保存到项目目录下命名为XXX
-    return optimal_distance,optimal_route                                       #输出最优路程长度和最优路径
+    print("本次独立运行 满足需求量：",optimal_demand)                               #打印需求满足量
+    print("本次独立运行 适应度：", optimal_fitness)                                   # 打印适应度
+    # plt.plot(Shortestlength_list)                                               #绘制单次运行若干次降温的最优长度折线图
+    # plt.savefig("SA+tabu KILLS TSP A single run length.png")                    #保存到项目目录下命名为XXX
+    # plt.clf()                                                                   #清空画布
+    plt.plot(Fitness_list)                                                      #绘制适应度曲线
+    plt.savefig("SA+tabu KILLS TSP A single run fitness.png")                   #保存到项目目录下命名为XXX
+    return optimal_distance,optimal_route,optimal_demand,optimal_fitness                 #输出最优路程长度和最优路径
 
 def main():
     Demand_matrix, Distance_matrix = read_excel_data(Demand_matrix_root, Distance_matrix_root)  #基础数据导入
-    independent_runs = 1                                                       #独立运行次数
+    independent_runs = 5                                                       #独立运行次数
     Several_independent_runs_distance_list = []                                 #若干次独立运行得到的最优路程长度表
     Several_independent_runs_route_list = []                                    #若干次独立运行得到的最优路径表
     work_book = xlwt.Workbook(encoding='utf-8')                                 #创建工作表
     sheet = work_book.add_sheet('1')                                            #添加sheet表，命名为XXX
     sheet.write(0,0,"独立运行次数")                                               #第0行第0列，赋值XXX
     sheet.write(0,1,"最优路程长度")                                               #第0行第1列，赋值XXX
-    sheet.write(0,2,"最优路径")
+    sheet.write(0,2,"最优路径")                                                  #第0行第2列，赋值XXX
+    sheet.write(0,3,"需求满足量")
+    sheet.write(0,4,"适应度")
     for i in range(independent_runs):                                           #独立运行 independent_runs 次
-        optimal_distance,optimal_route = SA(T_begin,T_end,alpha,L,Distance_matrix,Demand_matrix)#运行SA()，输出当次运行的最优长度和最优路线
+        optimal_distance,optimal_route,optimal_demand,optimal_fitness = SA(T_begin,T_end,alpha,L,Distance_matrix,Demand_matrix)#运行SA()，输出当次运行的最优长度和最优路线
         Several_independent_runs_distance_list.append(optimal_distance)         #记录当此运行的最优长度于列表中
         Several_independent_runs_route_list.append(optimal_route)               #记录当次运行的最优路径于列表中
         sheet.write(i+1,0,str(i+1))                                             #第 i+1 行第 0 列输入当次迭代的序号
         sheet.write(i+1,1,optimal_distance)                                     #第 i+1 行第 1 列输入当次迭代的最优长度
         sheet.write(i+1,2,str(optimal_route))                                   #第 i+1 行第 2 列输入当次迭代的最优路线
-    work_book.save('SA + tabu KILLS TSP.xls')                                   #保存工作表于项目目录下，命名为XXX
+        sheet.write(i+1,3,str(optimal_demand))
+        sheet.write(i+1,4,str(optimal_fitness))
+    work_book.save('SA + Tabu KILLS TSP ('+str(independent_runs)+' times).xls')                                   #保存工作表于项目目录下，命名为XXX
     plt.clf()                                                                   #清空画板
     plt.plot(Several_independent_runs_distance_list)                            #绘制若干次独立运行的最优路线长度折线图
-    plt.savefig("SA + tabu KILLS TSP")                                          #保存图片于项目目录下，命名为XXX
+    plt.savefig('SA + Tabu KILLS TSP ('+str(independent_runs)+' times')                                          #保存图片于项目目录下，命名为XXX
 
 if __name__ == '__main__':
     main()
