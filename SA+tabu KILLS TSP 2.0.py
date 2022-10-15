99ea5c4e40cba62df441e30a7cf49d1a1e03c61c
HandsomeQiDong <76278414+HandsomeQiDong@users.noreply.github.com>
2022-10-16 00:09:04 +0800

    version 2.2

--- SA+tabu KILLS TSP 2.0.py
+++ SA+tabu KILLS TSP 2.0.py
@@ -12,10 +12,10 @@ import matplotlib.pyplot as plt
 import xlrd
 import xlwt
 
-T_begin = 80000      #初始温度
+T_begin = 60000      #初始温度
 T_end = 15      #结束温度
 alpha = 0.99        #降温系数
-L = 2000        #迭代次数
+L = 3000        #迭代次数
 buildings = 19  #楼栋数量
 Demand_matrix_root = "D:\demo\yiwenchao finnal course\Demand matrix.xlsx"       #修改成Demand matrix.xlsx在自己电脑的路径
 Distance_matrix_root = "D:\demo\yiwenchao finnal course\Distance matrix.xlsx"   #同上
@@ -69,24 +69,26 @@ def creat_newRoute(old_route,tabu_list):
     tabu_list.append([change_a,change_b])                                                   #更新禁忌列表
     return new_route,tabu_list                                                              #输出新路线和禁忌列表
 
-#某路线的需求满足量计算函数
+#某路线的需求满足量计算函数 输入：车辆行驶速度、行驶路径、距离矩阵、需求矩阵
 def totaldemand(v,travel_route,Distance_matrix,Demand_matrix):
     current_time = 0                                                            #当前时间为0
     sum_demand = Demand_matrix[travel_route[0]][current_time+1]                 #初始需求为起点在第0时刻的需求
     for i in range(len(travel_route)-1):                                        #i = 0~17
-        frontpoint = int(travel_route[i])
-        latterpoint = int(travel_route[i+1])
-        distance_between = Distance_matrix[frontpoint][latterpoint]
-        time_cost = int(distance_between/v)
-        latterdemand = Demand_matrix[latterpoint][current_time+time_cost+1]
-        sum_demand = sum_demand + latterdemand
-        current_time += time_cost
-    return sum_demand
+        frontpoint = int(travel_route[i])                                       #记录相邻两点的前点
+        latterpoint = int(travel_route[i+1])                                    #记录相邻两点的后点
+        distance_between = Distance_matrix[frontpoint][latterpoint]             #记录两点之间的距离
+        time_cost = int(distance_between/v)                                     #计算从前点到后点的所需的时间
+        latterdemand = Demand_matrix[latterpoint][current_time+time_cost+1]     #计算车辆到达时后点的需求
+        sum_demand = sum_demand + latterdemand                                  #计算总需求满足量
+        current_time += time_cost                                               #更新当前时间
+    return sum_demand                                                           #输出总需求满足量
 
 #计算适应度函数(合理吗？？？？？)
 def fitness(route_distance, sum_demand):
-    all = route_distance + sum_demand
-    return 0.3*route_distance-7*sum_demand
+    di = (route_distance - 1212)/(5306 - 1212)
+    de = (sum_demand - 94)/(255 - 94)
+    f = (0.3 * di - 0.7 * de)*1000
+    return f
 
 #模拟退火算法
 def SA(T_begin,T_end,alpha,L,Distance_matrix,Demand_matrix):                    #T_begin初始温度;T_end终止温度;alpha降温系数;L迭代次数
@@ -118,7 +120,7 @@ def SA(T_begin,T_end,alpha,L,Distance_matrix,Demand_matrix):                    
         Shortestlength_list.append(Shortestlength)                              #把当前路径长度加入列表
         Fitness_list.append(fitness(Shortestlength,totaldemand(10,old_route,Distance_matrix,Demand_matrix)))
                                                                                 #把当前适应度加入列表
-        # print(cnt,"次降温，温度为：",T," 路程长度为：", Shortestlength,"适应度：",fitness(Shortestlength,totaldemand(10,old_route,Distance_matrix,Demand_matrix)))
+        print(cnt,"次降温，温度为：",T," 路程长度为：", Shortestlength,"适应度：",fitness(Shortestlength,totaldemand(10,old_route,Distance_matrix,Demand_matrix)))
                                                                                 #报文
     optimal_route = old_route                                                   #当温度降到终止温度，把此时的旧路线作为最优路线
     optimal_distance = totaldistance(optimal_route,Distance_matrix)             #计算最优路线的路程长度
@@ -132,14 +134,15 @@ def SA(T_begin,T_end,alpha,L,Distance_matrix,Demand_matrix):                    
     # plt.savefig("SA+tabu KILLS TSP A single run length.png")                    #保存到项目目录下命名为XXX
     # plt.clf()                                                                   #清空画布
     plt.plot(Fitness_list)                                                      #绘制适应度曲线
-    plt.savefig("SA+tabu KILLS TSP A single run fitness.png")                   #保存到项目目录下命名为XXX
+    plt.savefig("SA+tabu KILLS TSP A single run fitness (1 times).png")                   #保存到项目目录下命名为XXX
     return optimal_distance,optimal_route,optimal_demand,optimal_fitness                 #输出最优路程长度和最优路径
 
 def main():
     Demand_matrix, Distance_matrix = read_excel_data(Demand_matrix_root, Distance_matrix_root)  #基础数据导入
-    independent_runs = 5                                                       #独立运行次数
+    independent_runs = 1                                                       #独立运行次数
     Several_independent_runs_distance_list = []                                 #若干次独立运行得到的最优路程长度表
     Several_independent_runs_route_list = []                                    #若干次独立运行得到的最优路径表
+    Several_independent_runs_fitness_list = []                                  #若干次独立运行得到的最优适应度函数表
     work_book = xlwt.Workbook(encoding='utf-8')                                 #创建工作表
     sheet = work_book.add_sheet('1')                                            #添加sheet表，命名为XXX
     sheet.write(0,0,"独立运行次数")                                               #第0行第0列，赋值XXX
@@ -151,6 +154,7 @@ def main():
         optimal_distance,optimal_route,optimal_demand,optimal_fitness = SA(T_begin,T_end,alpha,L,Distance_matrix,Demand_matrix)#运行SA()，输出当次运行的最优长度和最优路线
         Several_independent_runs_distance_list.append(optimal_distance)         #记录当此运行的最优长度于列表中
         Several_independent_runs_route_list.append(optimal_route)               #记录当次运行的最优路径于列表中
+        Several_independent_runs_fitness_list.append(optimal_fitness)           #记录当次运行的最优适应度函数于列表中
         sheet.write(i+1,0,str(i+1))                                             #第 i+1 行第 0 列输入当次迭代的序号
         sheet.write(i+1,1,optimal_distance)                                     #第 i+1 行第 1 列输入当次迭代的最优长度
         sheet.write(i+1,2,str(optimal_route))                                   #第 i+1 行第 2 列输入当次迭代的最优路线
@@ -158,7 +162,7 @@ def main():
         sheet.write(i+1,4,str(optimal_fitness))
     work_book.save('SA + Tabu KILLS TSP ('+str(independent_runs)+' times).xls')                                   #保存工作表于项目目录下，命名为XXX
     plt.clf()                                                                   #清空画板
-    plt.plot(Several_independent_runs_distance_list)                            #绘制若干次独立运行的最优路线长度折线图
+    plt.plot(Several_independent_runs_fitness_list)                            #绘制若干次独立运行的最优路线长度折线图
     plt.savefig('SA + Tabu KILLS TSP ('+str(independent_runs)+' times')                                          #保存图片于项目目录下，命名为XXX
 
 if __name__ == '__main__':
